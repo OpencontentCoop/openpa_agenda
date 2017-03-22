ae87b0ac99dddf9e42b57fd140d83c82ac6473ce
Luca Realdi <luca.realdi@opencontent.it>
2017-03-22 16:13:44 +0100

    no message

--- bin/php/generate_eztags_from_tcu.php
+++ bin/php/generate_eztags_from_tcu.php
@@ -20,11 +20,11 @@ $cli = eZCLI::instance();
 $user = eZUser::fetchByName('admin');
 eZUser::setCurrentlyLoggedInUser($user, $user->attribute('contentobject_id'));
 
-function createTag($name, $parentTagId, $locale)
+function createTag($name, $parentTagId, $locale = 'ita-IT')
 {
     global $cli;
+    global $tagRepository;
 
-    $tagRepository = new \Opencontent\Opendata\Api\TagRepository();
     $struct = new \Opencontent\Opendata\Api\Structs\TagStruct();
     $struct->parentTagId = $parentTagId;
     $struct->keyword = $name;
@@ -38,7 +38,16 @@ function createTag($name, $parentTagId, $locale)
     return $result['tag'];
 }
 
+$tagRepository = new \Opencontent\Opendata\Api\TagRepository();
+$locale = 'ita-IT';
 try {
+
+    try{
+        $root = $tagRepository->read('Tipologia di evento');
+    }catch(Exception $e){
+        $root = createTag('Tipologia di evento', 0);
+    }
+
     $treeNode = 'https://www.cultura.trentino.it/api/opendata/v1/content/node/200420';
     $treeUrl = $treeNode . '/list/offset/0/limit/1000';
 
@@ -49,22 +58,28 @@ try {
             $item = new OpenPAApiChildNode($item);
 
             $cli->output($item->objectName);
+            $itemTag = createTag($item->objectName, $root->id);
 
             foreach ($item->getChildren() as $child) {
 
                 $cli->output(' |- ' . $child->objectName);
+                $childTag = createTag($child->objectName, $itemTag->id);
 
                 foreach ($child->getChildren() as $subChild) {
 
                     $cli->output('     |- ' . $subChild->objectName);
+                    $subChildTag = createTag($subChild->objectName, $childTag->id);
 
                     foreach ($subChild->getChildren() as $subSubChild) {
 
                         $cli->output('         |- ' . $subSubChild->objectName);
+                        $subSubChildTag = createTag($subSubChild->objectName, $subChildTag->id);
                     }
                 }
             }
         }
+    }else{
+        throw new Exception("Url $treeUrl not found");
     }
 
     $script->shutdown();
--- doc/UPDATE-TO-1.5.md
+++ doc/UPDATE-TO-1.5.md
@@ -1,4 +1,23 @@
-## Modifica della classe event
-### Utilizzo di eztags invece di ezobjectrelationlist per attributo tipo_evento
+## Requisiti
+
+ - Versione minima openpa 2.11
+ - Versione minima eztags 2.2.1
+ - Versione minima ocopendata 2.8
+
+### Utilizzo di eztags invece di ezobjectrelationlist o ezselection
+
+### Installare eztags
+
+`php extension/openpa/bin/php/check_eztags.php -sala_backend`
+
+Se risponde "Non installato"
+
+`php extension/openpa/bin/php/install_sql.php -sala_backend --file=/home/httpd/comunweb/html/extension/eztags/sql/postgresql/schema.sql --run`
+
+### Installare alberatura tags tcu
+
+`php extension/openpa_agenda/bin/php/generate_eztags_from_tcu.php -sala_backend`
+
+### Modifica della classe event
 
 - modificare l'identificatore tipo_evento in tipo_evento_relation
