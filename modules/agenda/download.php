4d8f887d12ef8ddf10f347c8206534513629ca7b
Luca Realdi <lr@opencontent.it>
2017-03-23 18:46:43 +0100

    content download

--- modules/agenda/download.php
+++ modules/agenda/download.php
@@ -9,14 +9,8 @@ if ($latestProgram instanceof eZContentObjectTreeNode){
     try {
         $post = OCEditorialStuffHandler::instance('programma_eventi')->fetchByObjectId($latestProgram->attribute('contentobject_id'));
 
-        $fileHandler = eZBinaryFileHandler::instance();
-        $result = $fileHandler->handleDownload( $latestProgram->object(), $post->attribute('file_attribute'), eZBinaryFileHandler::TYPE_FILE );
-
-        if ( $result == eZBinaryFileHandler::RESULT_UNAVAILABLE )
-        {
-            eZDebug::writeError( "The specified file could not be found." );
-            return $Module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
-        }
+        $Module->redirectTo('content/download/' . $latestProgram->object()->attribute('id') . '/' . $post->attribute('file_attribute')->attribute('id') . '/file/agenda.pdf' );
+        return;
 
     }catch(Exception $e) {
         eZDebug::writeError( $e->getMessage() );
