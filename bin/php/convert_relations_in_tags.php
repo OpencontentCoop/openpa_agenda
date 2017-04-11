84cdc7b258661c0daa0afa5f4397ef4c1241df1a
Luca Realdi <lr@opencontent.it>
2017-04-11 09:32:02 +0200

    fix translations in convert script

--- bin/php/convert_relations_in_tags.php
+++ bin/php/convert_relations_in_tags.php
@@ -1,10 +1,12 @@
 <?php
 require 'autoload.php';
 
-$script = eZScript::instance( array( 'description' => ( "Converte relazioni oggetti in tag \n\n" ),
-                                     'use-session' => false,
-                                     'use-modules' => true,
-                                     'use-extensions' => true ) );
+$script = eZScript::instance(array(
+    'description' => ( "Converte relazioni oggetti in tag \n\n" ),
+    'use-session' => false,
+    'use-modules' => true,
+    'use-extensions' => true
+));
 
 $script->startup();
 
@@ -12,21 +14,21 @@ $options = $script->getOptions(
     '[class:][relations_attribute:][tags_attribute:][tag_root_id:]',
     '',
     array(
-        'class'  => 'Identificatore di classe',
-        'relations_attribute'  => 'Identificatore di classe',
-        'tags_attribute'  => 'Identificatore di classe',
-        'tag_root_id'  => 'Identificatore di classe'
+        'class' => 'Identificatore di classe',
+        'relations_attribute' => 'Identificatore di classe',
+        'tags_attribute' => 'Identificatore di classe',
+        'tag_root_id' => 'Identificatore di classe'
     )
 );
 
 $script->initialize();
-$script->setUseDebugAccumulators( true );
+$script->setUseDebugAccumulators(true);
 
 $cli = eZCLI::instance();
 
-OpenPALog::setOutputLevel( OpenPALog::ALL );
+OpenPALog::setOutputLevel(OpenPALog::ALL);
 
-function createTag($name, $parentTagId, $locale)
+function createTag($name, $parentTagId, $locale, eZContentObject $contentObject)
 {
     global $cli;
 
@@ -37,10 +39,33 @@ function createTag($name, $parentTagId, $locale)
     $struct->locale = $locale;
     $struct->alwaysAvailable = true;
 
+    $currentVersion = $contentObject->currentVersion();
+    $availableLanguages = $currentVersion->translationList(false, false);
+    if (count($availableLanguages) > 1) {
+        $struct->alwaysAvailable = false;
+    }
+
     $result = $tagRepository->create($struct);
-    if ($result['message'] == 'success'){
+    if ($result['message'] == 'success') {
         $cli->warning(' - Created tag ' . $name);
     }
+
+    $tagId = $result['tag']->id;
+
+    foreach ($availableLanguages as $languageCode) {
+        if ($languageCode != $locale) {
+            $language = \eZContentLanguage::fetchByLocale($languageCode);
+            $translation = new eZTagsKeyword(array(
+                'keyword_id' => $tagId,
+                'language_id' => $language->attribute('id'),
+                'keyword' => $currentVersion->name($languageCode),
+                'locale' => $languageCode,
+                'status' => eZTagsKeyword::STATUS_PUBLISHED
+            ));
+            $translation->store();
+        }
+    }
+
     return $result['tag'];
 }
 
@@ -48,41 +73,55 @@ function convertObject(eZContentObject $object)
 {
     global $cli, $options, $locale;
 
-    /** @var eZContentObjectAttribute[] $dataMap */
-    $dataMap = $object->attribute('data_map');
+    if ($object->attribute('main_node_id')) {
 
-    $tags = array();
+        /** @var eZContentObjectAttribute[] $dataMap */
+        $dataMap = $object->attribute('data_map');
 
-    if (isset($dataMap[$options['relations_attribute']]) && $dataMap[$options['relations_attribute']]->hasContent()){
-        /** @var \Opencontent\Opendata\Api\Values\Tag[] $tags */
-        $ids = explode('-', $dataMap[$options['relations_attribute']]->toString());
-        foreach($ids as $id){
-            $related = eZContentObject::fetch($id);
-            if ($related instanceof eZContentObject){
-                $name = $related->attribute('name');
-                if ($name != 'Evento singolo' && $name != 'Manifestazione')
-                    $tags[] = createTag($name, $options['tag_root_id'], $locale);
+        $tags = array();
+
+        if (isset( $dataMap[$options['relations_attribute']] ) && $dataMap[$options['relations_attribute']]->hasContent()) {
+            /** @var \Opencontent\Opendata\Api\Values\Tag[] $tags */
+            $ids = explode('-', $dataMap[$options['relations_attribute']]->toString());
+            foreach ($ids as $id) {
+                $related = eZContentObject::fetch($id);
+                if ($related instanceof eZContentObject) {
+                    $name = $related->attribute('name');
+                    if ($name != 'Evento singolo' && $name != 'Manifestazione') {
+                        $tags[] = createTag($name, $options['tag_root_id'], $locale, $related);
+                    }
+                }
             }
         }
-    }
-    if (isset($dataMap[$options['tags_attribute']])){
-        $idString = array();
-        $keywordString = array();
-        $parentString = array();
-        $localeString = array();
-        foreach($tags as $tag){
-            $idString[] = $tag->id;
-            $keywordString[] = $tag->keyword;
-            $parentString[] = $tag->parentId;
-            $localeString[] = $locale;
+        if (isset( $dataMap[$options['tags_attribute']] )) {
+
+            $currentVersion = $object->currentVersion();
+            $availableLanguages = $currentVersion->translationList(false, false);
+
+            foreach ($availableLanguages as $language) {
+
+                $localeDataMap = $object->fetchDataMap(false, $language);
+
+                $idString = array();
+                $keywordString = array();
+                $parentString = array();
+                $localeString = array();
+                foreach ($tags as $tag) {
+                    $idString[] = $tag->id;
+                    $keywordString[] = $tag->keywordTranslations[$language];
+                    $parentString[] = $tag->parentId;
+                    $localeString[] = $language;
+                }
+
+                $tagString = implode('|#', array_merge($idString, $keywordString, $parentString, $localeString));
+                $localeDataMap[$options['tags_attribute']]->fromString($tagString);
+                $localeDataMap[$options['tags_attribute']]->store();
+
+            }
         }
 
-        $tagString = implode( '|#', array_merge($idString, $keywordString, $parentString, $localeString) );
-        $dataMap[$options['tags_attribute']]->fromString($tagString);
-        $dataMap[$options['tags_attribute']]->store();
+        eZContentOperationCollection::registerSearchObject($object->attribute('id'));
     }
-
-    eZContentOperationCollection::registerSearchObject($object->attribute('id'));
 }
 
 /**
@@ -97,46 +136,45 @@ function getClassAttribute(eZContentClass $class, $identifier, $type)
 {
     $classAttribute = $class->fetchAttributeByIdentifier($identifier);
     if ($classAttribute instanceof eZContentClassAttribute
-        && $classAttribute->DataTypeString == $type){
+        && $classAttribute->DataTypeString == $type
+    ) {
         return $classAttribute;
     }
     throw new Exception("Attribute {$identifier} not found or not valid");
 }
 
-try
-{
+try {
     $locale = 'ita-IT';
     $class = eZContentClass::fetchByIdentifier($options['class']);
-    if ($class instanceof eZContentClass){
-        $relationsAttribute = getClassAttribute($class, $options['relations_attribute'], eZObjectRelationListType::DATA_TYPE_STRING);
+    if ($class instanceof eZContentClass) {
+        $relationsAttribute = getClassAttribute($class, $options['relations_attribute'],
+            eZObjectRelationListType::DATA_TYPE_STRING);
         $tagsAttribute = getClassAttribute($class, $options['tags_attribute'], eZTagsType::DATA_TYPE_STRING);
 
         $count = $class->objectCount();
         $offset = 0;
         $limit = 50;
         $i = 0;
-        do
-        {
+        do {
             /** @var eZContentObject[] $items */
-            $items = eZContentObject::fetchFilteredList( array( 'contentclass_id' => $class->attribute('id') ), $offset, $limit );
-            foreach($items as $object){
+            $items = eZContentObject::fetchFilteredList(array('contentclass_id' => $class->attribute('id')), $offset,
+                $limit);
+            foreach ($items as $object) {
                 $i++;
                 $cli->warning($i . '/' . $count . " " . $object->attribute('name'));
                 convertObject($object);
             }
             $offset = $offset + $limit;
-        } while ( count( $items ) == $limit );
+        } while (count($items) == $limit);
 
-    }else{
+    } else {
         throw new Exception("Class {$options['class']} not found");
     }
 
     $script->shutdown();
-}
-catch( Exception $e )
-{
+} catch (Exception $e) {
     $cli->error($e->getMessage());
     $errCode = $e->getCode();
     $errCode = $errCode != 0 ? $errCode : 1; // If an error has occured, script must terminate with a status other than 0
-    $script->shutdown( $errCode, $e->getMessage() );
+    $script->shutdown($errCode, $e->getMessage());
 }
